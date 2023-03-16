8e2e68d2c5eae2208e869b3cdbda785a7bad73a9
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-03-16 20:35:38 +0100

    let + assignments + expressions evaluatable

--- couch-lang/evaluator/src/lib.rs
+++ couch-lang/evaluator/src/lib.rs
@@ -4,16 +4,13 @@ use std::{
 };
 
 pub enum IdentifierType {
-    Value {
-        mutable: bool,
-        value: Node<Expression>,
-    },
-    Function {
-        value: Node<Statement>,
-    },
+    Value { mutable: bool, value: Value },
+    Function { value: Node<Statement> },
 }
 
-use couch_lang_parser::{BinaryVariant, Expression, Node, Position, Statement, UnaryVariant};
+use couch_lang_parser::{
+    AssignmentVariant, BinaryVariant, Expression, Node, Position, Statement, UnaryVariant,
+};
 
 pub mod value;
 use value::Value;
@@ -21,9 +18,84 @@ use value::Value;
 pub struct Evaluator {}
 
 impl Evaluator {
+    fn evaluate_statements(statements: Vec<Node<Statement>>) -> Option<Value> {
+        let mut outer_context = HashMap::new();
+        let mut inner_context = HashMap::new();
+        statements.into_iter().find_map(|statement| {
+            Self::evaluate_statement(statement, &mut outer_context, &mut inner_context)
+        })
+    }
+    fn evaluate_statement(
+        node: Node<Statement>,
+        outer_context: &mut HashMap<String, IdentifierType>,
+        inner_context: &mut HashMap<String, IdentifierType>,
+    ) -> Option<Value> {
+        match node.value {
+            Statement::Let {
+                mutable,
+                identifier,
+                value,
+            } => {
+                let Expression::Identifier(identifier) = identifier.value else {
+                    todo!("handle error");
+                };
+                let value = Self::evaluate_expression(*value, outer_context, inner_context);
+                inner_context.insert(identifier, IdentifierType::Value { mutable, value });
+                None
+            }
+            Statement::Return(_) => todo!(),
+            Statement::Error(_) => todo!(),
+            Statement::Assignment {
+                left,
+                right,
+                variant,
+            } => {
+                let Expression::Identifier(identifier) = left.value else {
+                    todo!("handle error");
+                };
+                let right = Evaluator::evaluate_expression(*right, outer_context, inner_context);
+                let Some(identifier_ref) = inner_context
+                    .get_mut(&identifier)
+                    .or(outer_context.get_mut(&identifier)) else {
+                        return Some(Value::Error { message: format!("identifier {identifier} not defined"), line: node.position.line, column: node.position.column });
+                };
+                match identifier_ref {
+                    IdentifierType::Value { mutable, value } => {
+                        if *mutable {
+                            match variant {
+                                AssignmentVariant::Base => *value = right,
+                                AssignmentVariant::Addition => *value += right,
+                                AssignmentVariant::Subtraction => *value -= right,
+                                AssignmentVariant::Multiplication => *value *= right,
+                                AssignmentVariant::Division => *value /= right,
+                            };
+                            None
+                        } else {
+                            Some(Value::Error {
+                                message: format!("identifier {identifier} is not mutable"),
+                                line: node.position.line,
+                                column: node.position.column,
+                            })
+                        }
+                    }
+                    IdentifierType::Function { .. } => Some(Value::Error {
+                        message: format!("function definitions are not mutable"),
+                        line: node.position.line,
+                        column: node.position.column,
+                    }),
+                }
+            }
+            Statement::Expression(expression) => Some(Self::evaluate_expression(
+                expression,
+                outer_context,
+                inner_context,
+            )),
+        }
+    }
     fn evaluate_binary_expression(
         node: Node<Expression>,
-        parent_context: &mut HashMap<String, IdentifierType>,
+        outer_context: &mut HashMap<String, IdentifierType>,
+        inner_context: &mut HashMap<String, IdentifierType>,
     ) -> Value {
         let Expression::Binary { left, right, variant } = node.value else {
             panic!("expected Binary, got {:#?}", node.value);
@@ -31,8 +103,12 @@ impl Evaluator {
 
         macro_rules! impl_variant {
             ($func_name:ident) => {
-                Self::evaluate_expression(*left, parent_context)
-                    .$func_name(Self::evaluate_expression(*right, parent_context))
+                Self::evaluate_expression(*left, outer_context, inner_context)
+                    .$func_name(Self::evaluate_expression(
+                        *right,
+                        outer_context,
+                        inner_context,
+                    ))
                     .unwrap_or_else(|message| Value::Error {
                         message,
                         line: node.position.line,
@@ -47,24 +123,26 @@ impl Evaluator {
             BinaryVariant::Multiplication => impl_variant!(mul),
             BinaryVariant::Division => impl_variant!(sub),
             BinaryVariant::Equal => Value::Bool(
-                Self::evaluate_expression(*left, parent_context)
-                    == Self::evaluate_expression(*right, parent_context),
+                Self::evaluate_expression(*left, outer_context, inner_context)
+                    == Self::evaluate_expression(*right, outer_context, inner_context),
             ),
             BinaryVariant::NotEqual => Value::Bool(
-                Self::evaluate_expression(*left, parent_context)
-                    != Self::evaluate_expression(*right, parent_context),
+                Self::evaluate_expression(*left, outer_context, inner_context)
+                    != Self::evaluate_expression(*right, outer_context, inner_context),
             ),
         }
     }
     pub fn evaluate_expression(
         expression: Node<Expression>,
-        parent_context: &mut HashMap<String, IdentifierType>,
+        outer_context: &mut HashMap<String, IdentifierType>,
+        inner_context: &mut HashMap<String, IdentifierType>,
     ) -> Value {
         match expression.value {
             Expression::Integer(v) => Value::Integer(v),
             Expression::Float(v) => Value::Float(v),
             Expression::Unary { subject, variant } => {
-                let evaluated_subject = Self::evaluate_expression(*subject, parent_context);
+                let evaluated_subject =
+                    Self::evaluate_expression(*subject, outer_context, inner_context);
                 match (variant, evaluated_subject) {
                     (UnaryVariant::NegateNumber, Value::Integer(v)) => Value::Integer(-v),
                     (UnaryVariant::NegateNumber, Value::Float(v)) => Value::Float(-v),
@@ -102,16 +180,14 @@ impl Evaluator {
                 }
             }
             Expression::Binary { .. } => {
-                Self::evaluate_binary_expression(expression, parent_context)
+                Self::evaluate_binary_expression(expression, outer_context, inner_context)
             }
             Expression::Call {
                 subject: _,
                 arguments: _,
             } => todo!(),
-            Expression::Identifier(q) => match parent_context.get(&q) {
-                Some(IdentifierType::Value { value, .. }) => {
-                    Self::evaluate_expression(value.clone(), parent_context)
-                }
+            Expression::Identifier(q) => match inner_context.get(&q).or(outer_context.get(&q)) {
+                Some(IdentifierType::Value { value, .. }) => value.clone(),
                 Some(IdentifierType::Function { value: _ }) => todo!(),
                 None => Value::Error {
                     message: format!("identifier {q} is not yet given value"),
@@ -119,13 +195,6 @@ impl Evaluator {
                     column: expression.position.column,
                 },
             },
-            Expression::Assignment {
-                left,
-                right,
-                variant,
-            } => {
-                todo!("{left:?} {variant:?} {right:?} ")
-            }
             Expression::Error(message) => {
                 let Position { line, column, .. } = expression.position;
                 Value::Error {
@@ -152,8 +221,9 @@ mod tests {
         let lexer = Lexer::new(input.chars());
         let mut parser = Parser::new(lexer.into_iter(), input.clone());
         let expression = parser.parse_expression();
-        let mut id_table = HashMap::new();
-        let value = Evaluator::evaluate_expression(expression, &mut id_table);
+        let mut id_table_0 = HashMap::new();
+        let mut id_table_1 = HashMap::new();
+        let value = Evaluator::evaluate_expression(expression, &mut id_table_0, &mut id_table_1);
         assert_eq!(
             6,
             match value {
@@ -165,26 +235,26 @@ mod tests {
 
     #[test]
     fn identifier() {
-        let input = String::from("a 5");
+        let input = String::from("a");
         let lexer = Lexer::new(input.chars());
 
         let mut parser = Parser::new(lexer.into_iter(), input.clone());
         let expression = parser.parse_expression();
-        let value = parser.parse_expression();
 
-        let mut id_table = HashMap::from([(
+        let mut id_table_0 = HashMap::from([(
             "a".to_string(),
             IdentifierType::Value {
-                value,
+                value: Value::Integer(5),
                 mutable: false,
             },
         )]);
-        let value = Evaluator::evaluate_expression(expression, &mut id_table);
+        let mut id_table_1 = HashMap::new();
+        let value = Evaluator::evaluate_expression(expression, &mut id_table_0, &mut id_table_1);
         assert_eq!(
             5,
             match value {
                 Value::Integer(v) => v,
-                value => panic!("expected Bool, got {value:#?}"),
+                value => panic!("expected Integer, got {value:#?}"),
             }
         )
     }
@@ -197,8 +267,9 @@ mod tests {
         let mut parser = Parser::new(lexer.into_iter(), input.clone());
         let expression = parser.parse_expression();
 
-        let mut id_table = HashMap::new();
-        let value = Evaluator::evaluate_expression(expression, &mut id_table);
+        let mut id_table_0 = HashMap::new();
+        let mut id_table_1 = HashMap::new();
+        let value = Evaluator::evaluate_expression(expression, &mut id_table_0, &mut id_table_1);
         assert_eq!(
             false,
             match value {
@@ -216,8 +287,9 @@ mod tests {
         let mut parser = Parser::new(lexer.into_iter(), input.clone());
         let expression = parser.parse_expression();
 
-        let mut id_table = HashMap::new();
-        let value = Evaluator::evaluate_expression(expression, &mut id_table);
+        let mut id_table_0 = HashMap::new();
+        let mut id_table_1 = HashMap::new();
+        let value = Evaluator::evaluate_expression(expression, &mut id_table_0, &mut id_table_1);
         assert_eq!(
             true,
             match value {
@@ -233,8 +305,9 @@ mod tests {
         let lexer = Lexer::new(input.chars());
         let mut parser = Parser::new(lexer.into_iter(), input.clone());
         let expression = parser.parse_expression();
-        let mut id_table = HashMap::new();
-        let error = Evaluator::evaluate_expression(expression, &mut id_table);
+        let mut id_table_0 = HashMap::new();
+        let mut id_table_1 = HashMap::new();
+        let error = Evaluator::evaluate_expression(expression, &mut id_table_0, &mut id_table_1);
         assert_eq!(
             Value::Error {
                 message: "no implementation exists for float + integer".to_string(),
@@ -243,5 +316,15 @@ mod tests {
             },
             error
         );
+    }
+
+    #[test]
+    fn evaluate_let_chain() {
+        let input = String::from("let mut a = 5; a += 5; a;");
+        let lexer = Lexer::new(input.chars());
+        let mut parser = Parser::new(lexer.into_iter(), input.clone());
+        let statements = parser.parse_statements();
+        let error = Evaluator::evaluate_statements(statements);
+        assert_eq!(Some(Value::Integer(10)), error);
     }
 }
--- couch-lang/evaluator/src/value.rs
+++ couch-lang/evaluator/src/value.rs
@@ -1,9 +1,9 @@
 use std::{
     fmt::Display,
-    ops::{Add, Div, Mul, Sub},
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign},
 };
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Value {
     Integer(i64),
     Float(f64),
@@ -41,7 +41,26 @@ macro_rules! implement_operator {
     };
 }
 
+macro_rules! implement_operator_assign {
+    ($func_trait:ident, $func_name:ident, $op:tt) => {
+        impl $func_trait for Value {
+            fn $func_name(&mut self, rhs: Self) {
+                match (self, rhs) {
+                    (Value::Integer(a), Value::Integer(b)) => {*a $op b},
+                    (Value::Float(a), Value::Float(b)) => {*a $op b},
+                    (a, b) => panic!("no implementation exists for {a} {}= {b}", stringify!($op)),
+                }
+            }
+        }
+    };
+}
+
 implement_operator!(Add, add, +);
 implement_operator!(Sub, sub, -);
 implement_operator!(Mul, mul, *);
 implement_operator!(Div, div, /);
+
+implement_operator_assign!(AddAssign, add_assign, +=);
+implement_operator_assign!(SubAssign, sub_assign, -=);
+implement_operator_assign!(MulAssign, mul_assign, *=);
+implement_operator_assign!(DivAssign, div_assign, /=);
