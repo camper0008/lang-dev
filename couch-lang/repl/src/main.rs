95454c446bfdd0c5f4b034a71d4adc9fab0e956f
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-03-16 15:14:36 +0100

    repl

---
+++ couch-lang/repl/src/main.rs
@@ -0,0 +1,32 @@
+use std::{
+    collections::HashMap,
+    env,
+    io::{stdin, stdout, BufRead, Write},
+};
+
+use couch_lang_evaluator::Evaluator;
+use couch_lang_lexer::{Lexer, Token};
+use couch_lang_parser::Parser;
+
+fn main() -> ! {
+    loop {
+        let mut stdin = stdin().lock();
+        let mut buf = String::new();
+        let args: Vec<String> = env::args().collect();
+        print!("> ");
+        stdout().flush().unwrap();
+        stdin.read_line(&mut buf).unwrap();
+        let lexer = Lexer::new(buf.chars());
+        let tokens: Vec<Token> = lexer.into_iter().collect();
+        if args.contains(&String::from("--tokens")) {
+            println!("tokens -> {tokens:#?}");
+        }
+        let mut parser = Parser::new(tokens.into_iter(), buf.clone());
+        let ast = parser.parse_expression();
+        if args.contains(&String::from("--ast")) {
+            println!("ast -> {ast:#?}");
+        }
+        let value = Evaluator::evaluate_expression(ast, &mut HashMap::new());
+        println!("value -> {value:?}");
+    }
+}
